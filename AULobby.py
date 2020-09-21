c5b1ea2e3b0c81ada639974f9032f04d824f8c8e
Thomas Scheer <thomas.k.scheer@gmail.com>
2020-09-21 13:09:51 -0500

    added help info

--- AULobby.py
+++ AULobby.py
@@ -92,9 +92,9 @@ async def help(ctx):
     "***NOTE:   only people with the `Server Voter` role can use this command.*** \n"
     "to get the `Server Voter` tag, vote here:\n"
     "https://top.gg/servers/734164220911812618/vote\n\n\n\n"
-    "New Aliases added\n"
+    "```New Aliases added\n"
     "Make = m\n"
     "Get = g\n"
-    "Help = h")
+    "Help = h```")
 
 client.run(DISCORD_TOKEN)
